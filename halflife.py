a1d6fecfd2725819e571bba3cf7ae146c8de6a17
timurbakibayev <timurbakibayev@gmail.com>
2018-11-17 13:09:48 +0600

    step 2

--- halflife.py
+++ halflife.py
@@ -1,5 +1,11 @@
 import pygame
 import random
+
+def show(a):
+    print("----")
+    for i in a:
+        print(i)
+
 def drawfield(a, screen):
     zoom = 30
     for i in range(len(a)):
@@ -8,49 +14,36 @@ def drawfield(a, screen):
                              [j*zoom,i*zoom, zoom, zoom], 0)
 
 
+def around(a):
+    a = a.copy()
+    empty_line = [[0 for i in range(len(a[0])+2)]]
+    for i in range(len(a)): a[i] = [0] + a[i] + [0]
+    return empty_line+a+empty_line
+
+def something(q,b):
+    for i in range(1,len(q)-1):
+        for j in range(1,len(q[i])-1):
+            k = - q[i][j] + sum([q[y][x] for y in range(i-1, i+2) for x in range(j-1,j+2)])
+            if k < 2 or k > 3: b[i-1][j-1] = 0
+            if k == 3: b[i-1][j-1] = 1
+
 def step(a):
     b = a.copy()
-
-    for i in range(len(a)):
-        for j in range(len(a[i])):
-            k = 0
-            if i > 0 and a[i-1][j]:
-                k += 1
-            if i < len(a)-1 and a[i+1][j]:
-                k += 1
-            if j > 0 and a[i][j-1]:
-                k += 1
-            if j < len(a[i])-1 and a[i][j+1]:
-                k += 1
-            if i > 0 and j > 0 and a[i-1][j-1]:
-                k += 1
-            if i > 0 and j < len(a[i])-1 and a[i-1][j+1]:
-                k += 1
-            if i < len(a)-1 and j > 0 and a[i+1][j-1]:
-                k += 1
-            if i < len(a)-1 and j < len(a[i])-1 and a[i+1][j+1]:
-                k += 1
-
-            if k < 2 or k > 3:
-                b[i][j] = 0
-
-
-
-
+    q = around(a.copy())
+    something(q,b)
     return b
 
 
 if __name__ == '__main__':
     pygame.init()
 
-    size = (400, 500)
+    size = (500, 500)
     screen = pygame.display.set_mode(size)
 
     pygame.display.set_caption("Half Life")
 
     done = False
     clock = pygame.time.Clock()
-    x = 1
     fps = 2
     counter = 0
 
@@ -59,9 +52,15 @@ if __name__ == '__main__':
     a = [
             [0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0],
-            [0,0,1,0,0,1,0,0,0],
-            [0,0,0,0,1,0,0,0,0],
+            [0,0,1,0,1,1,0,0,0],
+            [0,1,1,0,1,1,0,0,0],
+            [0,0,1,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,1,0,1,0,0,0,0],
+            [0,0,1,0,1,0,0,0,0],
+            [0,0,1,0,1,0,0,0,0],
         ]
 
     # Loop as long as done == False
